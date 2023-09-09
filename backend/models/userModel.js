08136c3fe8d4cb95034f71c32a64f0161ae4a242
uthmanduro <udurosinlohun@gmail.com>
2023-09-09 23:36:36 +0100

    added trim data

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,44 +5,53 @@ const userSchema = new mongoose.Schema({
     firstname: {
         type: String,
         required: [true, 'A user must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
     lastname: {
         type: String,
         required: [true, 'A user must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
     email: {
         type: String,
         required: [true, 'A user must have an email'],
         unique: true,
         lowercase: true,
-        validate: [validator.isEmail, 'Please provide a valid email'] 
+        validate: [validator.isEmail, 'Please provide a valid email'],
+        trim: true
     },
     password: {
         type: String,
         required: [true, 'A user must have a password'],
-        minlength: 8
+        minlength: 8,
+        trim: true
     },
     passwordConfirm: {
         type: String,
         required: [true, 'A user must have a password'],
-        minlength: 8
+        minlength: 8,
+        trim: true
     },
     username: {
         type: String,
         required: [true, 'A user must have a username'],
-        unique: true
+        unique: true,
+        trim: true
     },
     phone_number: {
         type: String,
-        unique: true
+        unique: true,
+        trim: true
     },
     bio: {
-        type: String
+        type: String,
+        trim: true
     },
     photo: {
-        type: String
+        type: String,
+        trim: true
     }
 });
 
