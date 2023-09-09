d0a2aecf188156fd0857d95f2cb6acb4a3fe6533
uthmanduro <udurosinlohun@gmail.com>
2023-09-09 23:32:44 +0100

    updtaed user model with firstname and last name and trim data

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    name: {
+    firstname: {
+        type: String,
+        required: [true, 'A user must have a name'],
+        unique: true
+    },
+    lastname: {
         type: String,
         required: [true, 'A user must have a name'],
         unique: true
@@ -12,7 +17,7 @@ const userSchema = new mongoose.Schema({
         required: [true, 'A user must have an email'],
         unique: true,
         lowercase: true,
-        validate: [validator.isEmail, 'Please provide a valid email']
+        validate: [validator.isEmail, 'Please provide a valid email'] 
     },
     password: {
         type: String,
