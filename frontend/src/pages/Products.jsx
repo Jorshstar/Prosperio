40b3718300739874e2b558a3ec6d29b92ef77157
samuelonoh <samuelonoh12345@gmail.com>
2023-10-02 11:34:43 +0100

    Resizing of the reset password page and correcting all the overflow in the dashboard

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -4,10 +4,10 @@ export default function Products() {
   
  
   return (
-    <div className="flex flex-col justify-center w-[80vw]">
-    <div className="flex items-start justify-center w-[90%] gap-10 mt-5">
-      <div className="bg-white rounded-lg shadow-lg w-[45%] h-[100%] flex flex-col items-center justify-center ">
-        <div className="h-[100%] w-[95%] ">
+    <div className="flex flex-col justify-center w-[80vw] h-[85vh]">
+    <div className="flex items-start justify-center w-[100%] h-[100%] gap-10 mt-5">
+      <div className="bg-white rounded-lg shadow-lg w-[45%] flex flex-col items-center justify-center">
+        <div className=" w-[95%] ">
           <h2 className="text-2xl font-bold ">Add Products</h2>
 
           <p className="font-bold border-t-2 border-b-2 border-gray-300 my-2">
@@ -46,21 +46,19 @@ export default function Products() {
             <span className="text-gray-500 font-semibold ml-3">#6000</span>
           </p>
           <b>Description:</b>
-          <p className="text-gray-500 font-semibold">
+          <div className="text-gray-500 font-semibold">
             Milo is a popular chocolate malt beverage known for its harmonious
             blend of chocolate and malt flavors with a hint of caramel.
             Enjoyed hot or cold, it combines cocoa, malted barley, and milk
             solids to create a comforting and energizing drink. Often
             fortified with essential vitamins and minerals, Milo provides both
-            a delicious taste and nutritional value. Its a beloved beverage
-            among people of all ages and can be enjoyed on its own or used in
-            various recipes, making it a versatile and cherished drink.
-          </p>
+            a delicious taste and nutritional value. 
+          </div>
         </div>
       </div>
-      <div className="w-[45%]">
+      <div className="w-[45%] h-[100%]">
         <p className='text-xl'>Product Image:</p>
-        <img src={milo} alt="Image of a milo" />
+        <img src={milo} alt="Image of a milo" className='h-[92%] w-[100%]' />
       </div>
     </div>
   </div>
